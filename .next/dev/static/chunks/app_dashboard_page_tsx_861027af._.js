4b7580a3d897f7706df484b1e7641caea3a2158f
anandkumar16 <anandyadav16022004@gmail.com>
2025-12-13 02:30:06 +0530

    Remove build artifacts

--- .next/dev/static/chunks/app_dashboard_page_tsx_861027af._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_a8c6534f._.js",
-  "static/chunks/node_modules_0b9fbdee._.js"
-],
-    source: "dynamic"
-});
